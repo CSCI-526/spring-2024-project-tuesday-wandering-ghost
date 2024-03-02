949e56ba06735e1518bc2b98a9f8af75c5e8cdc0
ryanwang <zxwang0331@gmail.com>
2024-03-01 22:33:04 -0800

    Fix the mask shrinking bug, modify the narrowing field to become black, add the limit of field recovery times, and design magic fruit to increase field recovery times

--- Assets/Scripts/MaskControllerTest.cs
+++ Assets/Scripts/MaskControllerTest.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MaskControllerTest : MonoBehaviour
 {
     public Transform player;
     public SpriteMask mask;
     public KeyCode toggleKey = KeyCode.Space;
-    //public float duration = 3f;
+    public float duration = 3f;
     public Vector3 targetScale = new Vector3(20, 20, 20);
-    
+    public TextMeshProUGUI counterText;
+
     Vector3 shrink = new Vector3(0.01f,0.01f,0.01f);
     Vector3 expandToOriginal = new Vector3(1f, 1f, 1f);
+    public int spacePressCount = 0;
+    public int maxPressCount = 3;
+    private bool isShrinking = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,16 +28,20 @@ public class MaskControllerTest : MonoBehaviour
     void Update()
     {
         transform.position = player.position;
-        if (Input.GetKeyDown(toggleKey))
+        if (Input.GetKeyDown(toggleKey) && spacePressCount < maxPressCount)
         {
             StartCoroutine(ResetMask());
-            ResetMask();
+            spacePressCount++;
         }
+        counterText.text = "Times: " + (maxPressCount - spacePressCount).ToString();
     }
 
     void FixedUpdate()
     {
-        ShrinkMask();
+        if (isShrinking)
+        {
+            ShrinkMask();
+        }
     }
 
     void ShrinkMask()
@@ -41,11 +51,13 @@ public class MaskControllerTest : MonoBehaviour
 
     IEnumerator ResetMask()
     {
-        for (int i = 0; i < 20; i++)
-        {
-            mask.transform.localScale += expandToOriginal;
-            yield return new WaitForSeconds(0.02f);
-        }
-
+        isShrinking = false;
+        mask.transform.localScale = targetScale;
+        yield return new WaitForSeconds(5);
+        isShrinking = true;
+    }
+    public void IncreaseMaxPressCount()
+    {
+        maxPressCount++;
     }
 }
