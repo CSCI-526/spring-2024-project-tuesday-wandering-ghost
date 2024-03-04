f403231b27330ff3af33722b78f6c4abf8234fbe
April Yao <yaoapril52@gmail.com>
2024-03-03 23:13:19 -0800

    [Add] data collection script for analytics
    1. Add 'Firebase' element in level scenes to collect data using script.
    
    2. Add 'FirebaseDataCollect.cs' to specify what data to collect and post to Firebase realtime database.
    
    3. Add 'GameData.cs' as level data structure.
    
    4. Modify 'Goal.cs' to call the post data function when trigger entered.

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -7,6 +7,7 @@ public class Goal : MonoBehaviour
     // Start is called before the first frame update
     public GameObject winPanel;
     public PlayerController playerController;
+    public FirebaseDataCollect firebaseData;
     void Start()
     {
         GameObject playerObject = GameObject.FindWithTag("Player");
@@ -26,5 +27,8 @@ public class Goal : MonoBehaviour
     {
         winPanel.SetActive(true);
         playerController.SetPlayerMovement(false);
+
+        // post data to firebase
+        firebaseData.FinishLevel();
     }
 }
