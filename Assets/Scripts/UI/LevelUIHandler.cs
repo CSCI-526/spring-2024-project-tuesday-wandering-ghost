7634f45ae0f252afa1c56f56c67994144e2e9cfe
Pipy-Weng <pipidashen@gmail.com>
2024-03-04 21:42:53 -0800

    UI fix, bug fix

--- Assets/Scripts/UI/LevelUIHandler.cs
+++ Assets/Scripts/UI/LevelUIHandler.cs
@@ -79,5 +79,9 @@ public class LevelUIHandler : MonoBehaviour
         SceneManager.LoadScene("LevelOne");
     }
 
+    public void StartLevelTwo()
+    {
+        SceneManager.LoadScene("LevelTwo");
+    }
 
 }
