029153742382ecc616ec43a05849c875ee3af20b
JavaPersuader <liuchuning831@gmail.com>
2024-03-04 20:49:25 -0800

    update levelone to level2

--- Assets/Scripts/UI/LevelUIHandler.cs
+++ Assets/Scripts/UI/LevelUIHandler.cs
@@ -78,6 +78,10 @@ public class LevelUIHandler : MonoBehaviour
     {
         SceneManager.LoadScene("LevelOne");
     }
+    public void StartLevelTwo()
+    {
+        SceneManager.LoadScene("LevelTwo");
+    }
 
 
 }
