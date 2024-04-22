aab2b931137364d24224469e8e271d2419a27a4c
April Yao <yaoapril52@gmail.com>
2024-04-21 21:45:48 -0700

    [Fixed] L4 trigger

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -11,7 +11,7 @@ public class Trigger : MonoBehaviour
     {
         triggerOnChild = transform.GetChild(0).gameObject;
         triggerOnChild.SetActive(false);
-         audioSource = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,8 +22,19 @@ public class Trigger : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // print("In Trigger");
         if (collision.CompareTag("Possessible") || collision.CompareTag("Player")) {
-            
+            // print("in active");
+            triggerOnChild.SetActive(true);
+            audioSource.Play();
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // print("In collision");
+        if (collision.gameObject.CompareTag("Possessible") || collision.gameObject.CompareTag("Player")) {
+            // print("child true");
             triggerOnChild.SetActive(true);
             audioSource.Play();
         }
